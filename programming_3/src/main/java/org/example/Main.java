9c27f16fa5a89d0e64bc29b10b7133bd9cd69d04
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-31 10:54:31 +0200

    Main adding

---
+++ programming_3/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+
+        
+
+
+    }
+}
